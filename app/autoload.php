a75edbe92491f0d5b771f96bb7a6ecacbb7b4907
Ph3nol <ph3@slynett.com>
2011-08-26 16:14:17 +0200

    DoctrineFixturesBundle and StofDoctrineExtensionsBundle installation

--- app/autoload.php
+++ app/autoload.php
@@ -14,6 +14,12 @@ $loader->registerNamespaces(array(
     'Monolog'          => __DIR__.'/../vendor/monolog/src',
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
+    
+    'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-fixtures/lib',
+    'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
+    
+    'Stof'  => __DIR__.'/../vendor/bundles',
+    'Gedmo' => __DIR__.'/../vendor/gedmo-doctrine-extensions/lib',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
