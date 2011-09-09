aa57ddf8a750af5a977efd81a6140442b5d15f2f
Ph3nol <ph3@slynett.com>
2011-09-09 20:23:23 +0200

    Some updates

--- src/Sly/ContentBundle/Resources/public/js/content.js
+++ src/Sly/ContentBundle/Resources/public/js/content.js
@@ -2,10 +2,10 @@ jQuery(document).ready(function($){
 
 });
 
-var menuYloc = -200;
+/*var menuYloc = -200;
 
 $(window).scroll(function () { 
 	offset = menuYloc+$(document).scrollTop();
 	if(offset<0){offset=0;}
 	$('.p_sidebar').animate({top:offset+"px"},{duration:500,queue:false});
-});
+});*/
